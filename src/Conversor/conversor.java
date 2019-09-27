67355b80ec08624df2db16599918caabb9dd7a58
Usuario DAM 1 <Usuario DAM 1@DESKTOP-R6MG806>
2019-09-27 13:01:10 +0200

    Todos los cambios estan correctos

--- src/Conversor/conversor.java
+++ src/Conversor/conversor.java
@@ -13,6 +13,7 @@ import java.util.Scanner;
  */
 public class conversor {
 
+
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         int euro = teclado.nextInt();
